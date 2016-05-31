56f6a6e474512bfc7e950c851bcdd658b8df105d
Juan Bacardit <juanbacardit@hotmail.es>
2016-05-31 14:09:44 +0200

    start point

---
+++ src/main.java
@@ -0,0 +1,12 @@
+package org.agilar.csd.ci.src;
+
+/**
+ * Created by juanbacarditbarrera on 31/5/16.
+ */
+public class main {
+
+    public static void Main() throws Exception{
+
+        System.out.println("hello world");
+    }
+}
